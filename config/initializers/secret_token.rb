4e3d29a75f08cd366b202e9387a7e974014cba85
Hugo Lopez <hugol.soul@gmail.com>
2015-01-22 21:53:09 -0600

    Fisrt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stukdo::Application.config.secret_key_base = 'a6652207a4078c0f1ef41fc4f0400b703a42b8b94774c78607c3dba4854f981370af66714b028f99347edb05376de760133969e4b44fdba0bf092297002e14b4'
