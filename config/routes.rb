722ccd0b10ed860dc52934c43ab3750e3c4f560d
Hugo Lopez <hugol.soul@gmail.com>
2015-01-23 17:52:58 -0600

    Created task scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Stukdo::Application.routes.draw do
+  resources :tasks
+
   devise_for :users
   #get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
