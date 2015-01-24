1db60f27a30146eb4b3b45dc1b38f2f1ead5ab49
Hugo Lopez <hugol.soul@gmail.com>
2015-01-23 22:14:55 -0600

    Added tasks custom action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Stukdo::Application.routes.draw do
-  resources :tasks
+  resources :tasks do
+      member do
+          put :change
+          end
+      end
 
   devise_for :users
   #get "pages/home"
